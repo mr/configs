cdcfab60416bef005ad83b095e8c00ed1d018a8c
Matt Robinson <mrobinson7627@gmail.com>
2013-06-23 12:15:31 -0400

    Added an installer

---
+++ installer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp -R ~/workspace/configs/awesometheme ~/.config/awesome/themes/matt
+cp ~/workspace/configs/rc.lua ~/.config/awesome/rc.lua
+cp ~/workspace/configs/tmux.conf ~/.tmux.conf
+cp ~/workspace/configs/vimrc ~/.vimrc
