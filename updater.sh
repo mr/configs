ad9e75e1ea5a2b4612bdf9dd618eb2f9afba4bff
mrobinson7627 <mrobinson7627@gmail.com>
2013-06-25 21:54:30 -0400

    Added Xresources

--- updater.sh
+++ updater.sh
@@ -4,3 +4,4 @@ cp -R ~/.config/awesome/themes/matt ~/workspace/configs/awesometheme
 cp ~/.config/awesome/rc.lua ~/workspace/configs/rc.lua
 cp ~/.tmux.conf ~/workspace/configs/tmux.conf
 cp ~/.vimrc ~/workspace/configs/vimrc
+cp ~/.Xresources ~/workspace/configs/Xresources
